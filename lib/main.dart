ba2bcdab4efdf9f3c68640eb09f1fbef031223f0
Afolayan Ademola Segun <54664998+kest-cloud@users.noreply.github.com>
2021-03-14 18:34:04 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: BallPage(),
+      ),
+    );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Ask Me Anything'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  void randomise() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+      print(ballNumber);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          randomise();
+        },
+        child: Image(
+          image: AssetImage('images/ball$ballNumber.png'),
+        ),
+      ),
+    );
+  }
+}
